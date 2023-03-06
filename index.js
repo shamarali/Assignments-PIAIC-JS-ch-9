f1d3d97b493d58e3e7249dff2559db9313f40dc9
Shamar Ali <111586356+shamarali@users.noreply.github.com>
2023-03-07 00:27:03 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let firstName = prompt ("Enter Your First Name");
+let lastName = prompt ("Enter Your Last Name");
+let age = prompt ("Enter Your age");
+
+let message = "Hello " + firstName + " " + lastName + " are You " + age + " Years old .";
+
+alert(message);
